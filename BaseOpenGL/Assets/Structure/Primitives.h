4c784c26ab03095a4bb2a2982f87b99d2320824e
Mathias Mohn Mørch <62551684+Skyress-s@users.noreply.github.com>
2023-02-25 15:57:31 +0100

    wokring on importing assimp

---
+++ BaseOpenGL/Assets/Structure/Primitives.h
@@ -0,0 +1,25 @@
+﻿#pragma once
+#include <vector>
+#include "Vertex.h"
+
+namespace KT
+{
+    namespace Primitives
+    {
+        
+        /**
+         * \brief 
+         * \param outVerts can be uninitialized 
+         * \param indices can be uninitialized
+         * \return 
+         */
+        void Cube(std::vector<Vertex>& outVerts, std::vector<int>& indices)
+        {
+            outVerts = std::vector<Vertex>();
+            indices = std::vector<int>();
+
+            const float u = 0.5f;
+            outVerts.push_back(Vertex());
+        }
+    }
+}
